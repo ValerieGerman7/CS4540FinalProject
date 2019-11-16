028c0f43afaa485007284b0cf70f01b27b933fa1
Cody W <cody.wint@gmail.com>
2019-11-15 17:45:36 -0700

    Search and OrderBy are done for courses. Need to make pagination better. Thinking of incoroprating what was done, with my paginate list class to make it easier.

--- CS4540PS2/Controllers/CourseController.cs
+++ CS4540PS2/Controllers/CourseController.cs
@@ -14,7 +14,7 @@ using System.Threading.Tasks;
 /// Course: CS 4540, University of Utah
 /// Copyright: CS 4540 and Valerie German - This work may not be copied for use in Academic Coursework.
 /// I, Valerie German, certify that I wrote this code from scratch and did not copy it in part or whole from another source. Any references used in the completion of this assignment are cited in my README file.
-/// File Contents: This file contains controller for admin course webpages - creating/editing/deleting courses and course overview.
+/// File Contents: This file contains controller for admin course webpages - creating/editing/deleting instances and course overview.
 /// </summary>
 namespace CS4540PS2.Controllers {
     [Authorize(Roles="Admin")]
@@ -32,11 +32,105 @@ namespace CS4540PS2.Controllers {
         /// Return the index page listing all course instances.
         /// </summary>
         /// <returns></returns>
-        public async Task<IActionResult> Index() {
-            var instances = _context.CourseInstance;
-            return View(await instances.ToListAsync());
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? pageNumber) {
+            // Set up the possible ordering schemes of the table.
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["CourseNumSortParam"] = String.IsNullOrEmpty(sortOrder) ? "course_num_desc" : "";
+            ViewData["CourseTitleSortParam"] = sortOrder == "title_asc" ? "title_desc" : "title_asc";
+            ViewData["DepartmentSortParam"] = sortOrder == "dept_asc" ? "dept_desc" : "dept_asc";
+            ViewData["SemesterSortParam"] = sortOrder == "semester_asc" ? "semester_desc" : "semester_asc";
+            ViewData["YearSortParam"] = sortOrder == "year_asc" ? "year_desc" : "year_asc";
+
+            // If there is a search string, filter by that, otherwise use the default
+            if (searchString != null) {
+                pageNumber = 1;
+            } else {
+                searchString = currentFilter;
+            }            
+            ViewData["CurrentFilter"] = searchString;
+
+            // get course instances
+            var instances = from c in _context.CourseInstance
+                            select c;
+
+            /* TODO: test if this is necessary*/
+            if (instances.Count() == 0)
+                return View();
+
+            // allow the user to search the course catalogue
+            instances = FilterBySearch(searchString, instances);
+
+            // reorder the results based on the selected filter
+            instances = OrderBySelection(sortOrder, instances);
+
+            //return View(await instances.ToListAsync());
+            int pageSize = 5;
+            return View(await PaginatedList<CourseInstance>.CreateAsync(instances.AsNoTracking(), pageNumber ?? 1, pageSize));
         }
-        
+
+        /// <summary>
+        /// Filters the courses returned by the database by a user supplied search string.
+        /// </summary>
+        private IQueryable<CourseInstance> FilterBySearch(string searchString, IQueryable<CourseInstance> courseInstances) {
+            // allow the user to search the course catalogue
+            if (!string.IsNullOrEmpty(searchString)) {
+                string[] searchWords = searchString.Split(' ');
+                foreach (string s in searchWords)
+                {
+                    if (!string.IsNullOrEmpty(s))
+                    {
+                        courseInstances = courseInstances.Where(c => c.Name.Contains(s)
+                            || c.Department.Contains(s)
+                            || c.Number.ToString().Contains(s)
+                            || c.Semester.Contains(s)
+                            || c.Year.ToString().Contains(s));
+                    }
+                }
+            }            
+            return courseInstances;
+        }
+
+        /// <summary>
+        /// Reorders the courses in the view based on user selection
+        /// </summary>
+        private IQueryable<CourseInstance> OrderBySelection(string sortOrder, IQueryable<CourseInstance> courseInstances) {
+            // reorder the results based on the selected filter           
+            switch (sortOrder)
+            {
+                case "course_num_desc":
+                    courseInstances = courseInstances.OrderByDescending(c => c.Number);
+                    break;
+                case "title_asc":
+                    courseInstances = courseInstances.OrderBy(c => c.Name);
+                    break;
+                case "title_desc":
+                    courseInstances = courseInstances.OrderByDescending(c => c.Name);
+                    break;
+                case "dept_asc":
+                    courseInstances = courseInstances.OrderBy(c => c.Department);
+                    break;
+                case "dept_desc":
+                    courseInstances = courseInstances.OrderByDescending(c => c.Department);
+                    break;
+                case "semester_asc":
+                    courseInstances = courseInstances.OrderBy(c => c.Semester);
+                    break;
+                case "semester_desc":
+                    courseInstances = courseInstances.OrderByDescending(c => c.Semester);
+                    break;
+                case "year_asc":
+                    courseInstances = courseInstances.OrderBy(c => c.Year);
+                    break;
+                case "year_desc":
+                    courseInstances = courseInstances.OrderByDescending(c => c.Year);
+                    break;
+                default:
+                    courseInstances = courseInstances.OrderBy(c => c.Number);
+                    break;
+            }
+            return courseInstances;
+        }
+
         /// <summary>
         /// GET for course create page.
         /// </summary>
---
+++ CS4540PS2/PaginatedList.cs
@@ -0,0 +1,57 @@
+﻿/*
+    Author:	Cody Winters
+    Date:	9/14/2019
+    Course:	CS 4540, University of Utah, School of Computing
+    Copyright:	CS 4540 and Cody Winters - This work may not be copied for use in Academic Coursework.
+
+    I Cody Winters, certify that I wrote this code from scratch and did not copy it in part or whole from
+    any other source.Any references used in the completion of the assignemnt are cited in my README file.
+
+    File Contents
+
+    This file allows each of the views to be paginated, meaning that you can specify how large the table 
+    for a give view (e.g. courses) should be, instead of always viewing all items.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CS4540PS2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CS4540PS2
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize) {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool HasNextPage {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
